08c02d2df9737af69d27663234a89e4a5d139dae
Safiul Haque <mdsafiulhaque4@gmail.com>
2024-04-13 11:27:31 +0530

    update

---
+++ backendSourceCode/ApiGateway/src/main/java/com/app/gateway/exceptions/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.app.gateway.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse {
+	private String message;
+	private boolean success;
+}
